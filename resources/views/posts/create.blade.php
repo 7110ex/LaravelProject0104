f269a13df255d6f6f605e4764495822d3e90e6d8
7110ex <n.reo.7110@gmail.com>
2025-01-23 02:30:12 +0000

    ブログ投稿編集画面作成、編集機能実装

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE HTML>
-<html lang="ja">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 <head>
     <meta charset="utf-8">
@@ -17,7 +17,7 @@
         </div>
         <div class="body">
             <h2>Body</h2>
-            <textarea name="post[body]" placeholder="今日も1日お疲れさまでした。" rows="40" cols="100">{{ old('post.body') }}</textarea>
+            <textarea name="post[body]" placeholder="今日も1日お疲れさまでした。">{{ old('post.body') }}</textarea>
             <p class="body__error" style="color:red">{{ $errors->first('post.body') }}</p>
         </div>
         <input type="submit" value="保存" />
